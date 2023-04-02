825119dbb627cf05dc58bb2be7aefff59f818abf
Priyanka <103273242+priyankaj04@users.noreply.github.com>
2023-04-02 10:44:39 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home"
+import Navbar from "./components/navbar";
+import Create from "./components/create";
+import Profile from "./components/profile";
+import About from "./components/about";
+import Categories from "./components/categories";
+import Signup from "./components/signup";
+import Login from "./components/login";
+import Granim from "react-granim";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Granim
+        direction={"diagonal"}
+        isPausedWhenNotInView={true}
+        stateTransitionSpeed={15000}
+        states={{
+          "default-state": {
+            gradients: [
+              ["#4e54c8", "#8f94fb"],
+              ["#7474bf", "#348ac7"],
+              ["#24c6dc","#514a9d"],
+              ["#4776e6", "#8e54e9"],
+              ["#2193b0", "#6dd5ed"]
+            ]
+          }
+        }}
+      />
+      <Routes>
+        <Route path="/" exact element={<Navbar />} />
+        <Route path="" exact element={<Home />} />
+        <Route path="/create" exact element={<Create />} />
+        <Route path="/profile" exact element={<Profile />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/categories" exact element={<Categories />} />
+        <Route path="/signup" exact element={<Signup />} />
+        <Route path="/login" exact element={<Login />} />
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import './index.css';
+import App from './App';
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient} >
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </BrowserRouter>
+);
+
